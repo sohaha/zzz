e1b2db6b16673b39eb0d179c06de1a0034a8927c
seekwe <seekwe@gmail.com>
2020-03-20 13:30:19 +0800

    misc: rename watch config name

--- util/util.go
+++ util/util.go
@@ -25,7 +25,7 @@ var (
 	once           sync.Once
 	installPath    string
 	homePath       string
-	Version        = "1.0.4"
+	Version        = "1.0.5"
 	BuildTime      = ""
 	BuildGoVersion = ""
 )
