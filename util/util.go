ead3974576e6d8ca829c7c1908f0499101a90cac
seekwe <seekwe@gmail.com>
2020-04-24 17:26:36 +0800

    fix: mac build

--- util/util.go
+++ util/util.go
@@ -25,7 +25,7 @@ var (
 	once           sync.Once
 	installPath    string
 	homePath       string
-	Version        = "1.0.10"
+	Version        = "1.0.11"
 	BuildTime      = ""
 	BuildGoVersion = ""
 )
