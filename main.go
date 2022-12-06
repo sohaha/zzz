8e98432852c6d11b84fb859d266e347385d5b016
seekwe <seekwe@gmail.com>
2022-12-06 17:35:30 +0800

    🚀 chore: Build perfect support

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"github.com/sohaha/zlsgo/zcli"
 	"github.com/sohaha/zzz/cmd"
+	"github.com/sohaha/zzz/util"
 )
 
+func init() {
+	zcli.Version = util.Version
+}
+
 func main() {
-	var c = make(chan struct{}, 0)
+	var c = make(chan struct{}, 1)
 	go cmd.GetNewVersion(c)
 	cmd.Execute()
-	// <-c
+	<-c
 }
