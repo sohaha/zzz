541c0e73e82f271e256f46590afeccb690d9f0dd
seekwe <seekwe@gmail.com>
2024-04-25 00:54:58 +0800

    🚀 chore: Update dependencies

--- util/util.go
+++ util/util.go
@@ -25,7 +25,7 @@ var (
 	once           sync.Once
 	installPath    string
 	homePath       string
-	Version        = "1.0.38"
+	Version        = "1.0.39"
 	BuildTime      = ""
 	BuildGoVersion = ""
 )
